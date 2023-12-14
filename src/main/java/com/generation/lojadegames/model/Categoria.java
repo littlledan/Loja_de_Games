2a754d3419fb4889e4896a9b8847d9919dec12fd
littlledan <daniel799.dl@gmail.com>
2023-12-14 12:24:36 -0300

    Criando .model, .controller, .repository e criando crud das categorias

---
+++ src/main/java/com/generation/lojadegames/model/Categoria.java
@@ -0,0 +1,41 @@
+package com.generation.lojadegames.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank(message = "O tipo da categoria não pode ser nulo ou conter apenas espaços em branco!")
+	@Size(min = 3, max = 20)
+	private String tipo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	
+}
